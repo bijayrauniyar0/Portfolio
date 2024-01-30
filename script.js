0c44cc4e2ed5c1727b2e28ae421ecc9760eb979e
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-01-30 16:29:53 +0545

    portfolio commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// your-script.js
+document.addEventListener('DOMContentLoaded', function () {
+  var typed = new Typed('#typed-output', {
+    strings: ["Web Developer", "Graphics Designer","Youtuber", "BCA Student"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    showCursor: false,
+    loop: true
+  });
+});
