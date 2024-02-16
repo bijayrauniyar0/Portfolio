00c83431390821eafd7568eb4057ad3d5d74876c
Bijay Rauniyar <rauniyarbijay0@gmail.com>
2024-02-16 13:59:45 +0545

    responsive

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ document.addEventListener('DOMContentLoaded', function () {
     loop: true
   });
 });
+
+const dropMenu = document.querySelector('.drop-menu')
+const toggleBtn = document.querySelector('.toggler')
+const toggler = document.querySelector('.toggler i')
+
+toggleBtn.onclick = function(){
+    dropMenu.classList.toggle('active')
+    const isOpen = dropMenu.classList.contains('active')
+
+    toggler.classList = isOpen
+    ? 'fa-solid fa-x'
+    :'fa-solid fa-bars'
+}
+
+
+
+const rating = document.getElementsByClassName('rating')[0];
+        const block = document.getElementsByClassName('block');
+        for ( var i = 1; i< 100; i++){
+            rating.innerHTML += "<div class ='block'></div>"
+            block[i].style.transform="rotate("+3.6 * i + "deg)"
+            block[i].style.animationDelay= `${i/40}s`;
+           
+        }
